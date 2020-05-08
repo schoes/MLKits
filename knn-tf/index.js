aa4e0c4ae38d446b4453dbe07454d7074d1bc54f
Sven Schoeni <sven.schoeni@gmail.com>
2020-05-08 10:09:20 +0200

    optimize code structure

--- knn-tf/index.js
+++ knn-tf/index.js
@@ -2,36 +2,46 @@
 const tf = require('@tensorflow/tfjs');
 const loadCsv = require('./load-csv');
 
-function knn(features, labels, predictionPoint, k) {
+const knn = (features, labels, predictionPoint, k) => {
+    const { mean, variance } = tf.moments(features, 0);
+    const scaledPredictionPoint = predictionPoint
+        .sub(mean)
+        .div(variance.pow(0.5));
     return (
         features
-            .sub(predictionPoint)
+            .sub(mean)
+            .div(variance.pow(0.5))
+            .sub(scaledPredictionPoint)
             .pow(2)
             .sum(1)
             .pow(0.5)
             .expandDims(1)
             .concat(labels, 1)
             .unstack()
-            .sort((a, b) => (a[0] > b[0] ? 1 : -1))
+            .sort((a, b) => (a.arraySync()[0] > b.arraySync()[0] ? 1 : -1))
             .slice(0, k)
-            .reduce((acc, pair) => acc + pair[1], 0) / k
+            .reduce((acc, pair) => acc + pair.arraySync()[1], 0) / k
     );
-}
+};
 
 let { features, labels, testFeatures, testLabels } = loadCsv(
     'kc_house_data.csv',
     {
         shuffle: true,
         splitTest: 10,
-        dataColumns: ['lat', 'long'],
+        dataColumns: ['lat', 'long', 'sqft_lot','sqft_living'],
         labelColumns: ['price'],
     }
 );
 
-const result = knn(
-    tf.tensor(features),
-    tf.tensor(labels),
-    tf.tensor(testFeatures[0]),
-    10
-);
-console.log(result, testLabels[0][0]);
+testFeatures.forEach((testPoint, i) => {
+    const result = knn(
+        tf.tensor(features),
+        tf.tensor(labels),
+        tf.tensor(testPoint),
+        10
+    );
+    const errorPercentage =
+        ((testLabels[i][0] - result) / testLabels[i][0]) * 100;
+    console.log('Error: ', errorPercentage);
+});
